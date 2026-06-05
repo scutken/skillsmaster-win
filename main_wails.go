37a67c9d2dcf5dc9d164d0b0870eefa3d004a06a
scutken <scutken@users.noreply.github.com>
2026-06-05 15:42:53 +0000

    fix: add debug logging to main_wails.go for startup diagnostics

--- main_wails.go
+++ main_wails.go
@@ -4,7 +4,11 @@
 package main
 
 import (
+	"context"
 	"embed"
+	"fmt"
+	"log"
+	"os"
 
 	"skillsmaster-win/models"
 
@@ -17,23 +21,46 @@ import (
 var assets embed.FS
 
 func main() {
+	// Set up logging to file so we can see errors
+	logFile, err := os.Create("skillsmaster-debug.log")
+	if err == nil {
+		log.SetOutput(logFile)
+		defer logFile.Close()
+	}
+
+	log.Println("SkillsMaster starting...")
+	wd, _ := os.Getwd()
+	log.Printf("Working directory: %s", wd)
+
 	resolver := models.NewDefaultPathResolver()
 	app := NewApp(resolver)
+	agents := app.GetAgents()
+	log.Printf("Loaded %d agents", len(agents))
 
-	err := wails.Run(&options.App{
-		Title:  "SkillsMaster",
-		Width:  1024,
-		Height: 768,
+	fmt.Printf("SkillsMaster starting with %d agents...\n", len(agents))
+
+	err = wails.Run(&options.App{
+		Title:     "SkillsMaster",
+		Width:     1024,
+		Height:    768,
+		MinWidth:  800,
+		MinHeight: 600,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        nil,
+		OnStartup: func(ctx context.Context) {
+			log.Println("Window started successfully")
+		},
 		Bind: []interface{}{
 			app,
 		},
 	})
+
 	if err != nil {
-		println("Error:", err.Error())
+		errMsg := fmt.Sprintf("SkillsMaster error: %v", err)
+		log.Println(errMsg)
+		fmt.Fprintln(os.Stderr, errMsg)
 	}
+	log.Println("SkillsMaster exiting")
 }
